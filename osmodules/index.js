8b754c8c57e4c2599b50398d68e9239044cd51bc
farid123d <117964049+farid123d@users.noreply.github.com>
2024-03-01 11:07:29 +0800

    os-path-done

---
+++ osmodules/index.js
@@ -0,0 +1,9 @@
+// os and path
+
+const os = require('os');
+console.log(os)
+// console.log(os.userInfo())
+console.log(os.homedir())
+console.log(os.hostname())
+console.log(os.totalmem())
+console.log(os.freemem())
---
+++ path/index.js
@@ -0,0 +1,9 @@
+const path = require('path')
+
+// uses of extension name
+const extensionName = path.extname('style.css')
+// console.log(extensionName)
+
+// uses of join function
+const joinName = path.join(__dirname + '../views');
+console.log(joinName)
