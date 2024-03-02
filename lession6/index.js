8201a380802b0b134fd923f42ab3c8ff913ee2fd
farid123d <117964049+farid123d@users.noreply.github.com>
2024-03-02 22:15:22 +0800

    node-route

--- lession6/index.js
+++ lession6/index.js
@@ -1,16 +1,68 @@
 const http = require('http')
+const fs = require('fs')
 
 const port = 3000
 const hostname = '127.0.0.1'
 
 // create the server
-const myServer = http.createServer((req, res) =>{
+const server = http.createServer((req, res) =>{
+ 
+   if(req.url === '/'){
+      fs.readFile('index.html', 'utf-8', (err, data) =>{
 
-   res.writeHead(200, {'Content-Type':'text/html'})
-   res.write('<H1>Hello</H1>')
-   res.end()
+         if(err){
+            console.log(err)
+         }
+         else{
+            res.writeHead(200, {'Content-Type': 'text/html'})
+            res.write(data)
+            res.end()
+         }
+      })
+   }
+
+   else if(req.url === '/about'){
+      fs.readFile('about.html', 'utf-8', (err, data) =>{
+         if(!err){
+            res.writeHead(200, { 'Content-Type': 'text/html' })
+            res.write(data)
+            res.end()
+         }
+         else{
+            console.log(err)
+         }
+      })
+   }
+
+   else if (req.url === '/contact') {
+      fs.readFile('contact.html', 'utf-8', (err, data) => {
+         if (!err) {
+            res.writeHead(200, { 'Content-Type': 'text/html' })
+            res.write(data)
+            res.end()
+         }
+         else {
+            console.log(err)
+         }
+      })
+   }
+
+   else {
+      fs.readFile('404.html', 'utf-8', (err, data) => {
+         if (!err) {
+            res.writeHead(200, { 'Content-Type': 'text/html' })
+            res.write(data)
+            res.end()
+         }
+         else {
+            console.log(err)
+         }
+      })
+   }
 })
 
-myServer.listen(port, hostname, () =>{
+
+// run the server
+server.listen(port, hostname, () =>{
    console.log(`Your server is running : http://${hostname}:${port}`)
 })
